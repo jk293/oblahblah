d3c818be15c75461f0028a37105a9f00d84707fc
jk293 <jk293@njit.edu>
2016-02-10 14:58:32 -0500

    Update phhhpp.php

--- phhhpp.php
+++ phhhpp.php
@@ -1,7 +1,7 @@
 <?php
-Name: <input type="text" name="name" value="<?php echo $name;?>">
+FirstName: <input type="text" Fname="name" value="<?php echo $Fname;?>">
 
-
+LastName : <input type="text" Lname="name" value="<?php echo $Lname;?>">
 
 
 
